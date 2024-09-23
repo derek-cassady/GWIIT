02516057b8290d0791cc3f0b1b90551615808409
Derek Cassady <derek@cassadyweb.com>
2024-09-23 13:15:56 -0400

    added custom 'Site' model to 'sites/models.py'

--- organizations/models.py
+++ organizations/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 class Organization(models.Model):
+    # Extensible fields    
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
     active = models.BooleanField(default=True)
+    
+    # Tracking fields
     date_created = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='created_organizations', null=True, blank=True)  # Reference to User model
     last_modified = models.DateTimeField(auto_now=True)
--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from organizations.models import Organization
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT, related_name='sites')  # Reference to Organization
+    
+    # Extensible fields
+    type = models.CharField(max_length=100, null=True, blank=True)  # Site type (e.g., Warehouse, Office, Clinic)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    
+    # Tracking fields
+    date_created = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, null=True, blank=True, related_name='created_sites')
+    last_modified = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, null=True, blank=True, related_name='modified_sites')
+
+    def __str__(self):
+        return self.name

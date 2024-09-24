adb16f97c1cae3342b502b3c9b1dc21f09e33561
derek-cassady <derek@cassadyweb.com>
2024-09-24 19:06:44 -0400

    Added 'SiteAdmin' to 'sites/admin.py' for 'Site' model.

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Site
 
-# Register your models here.
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'organization', 'site_type', 'address', 'active']
+    search_fields = ['name', 'organization', 'site_type']
+    list_filter = ['organization', 'active']
+    ordering = ['organization', 'name']

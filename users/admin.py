6495cef6348d513ed31932b3ef9c4b022c479f42
derek-cassady <derek@cassadyweb.com>
2024-09-24 19:34:16 -0400

    Added comments to 'users/admin.py'.

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,14 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
     list_display = ['email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff']
+    
+    # Fields that can be searched
     search_fields = ['email', 'username', 'first_name', 'last_name']
+    
+    # Fields to filter
     list_filter = ['is_active', 'is_staff', 'organization', 'site']
+    
+    # Fields to order by (default)
     ordering = ['email']

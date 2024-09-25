85d084c0fa6375b694f6d90f58896ef88a59f693
Derek Cassady <derek@cassadyweb.com>
2024-09-25 16:21:57 -0400

    Added 'verbose_name' and 'gettext_lazy' where appropriate in 'sites/apps.py'

--- sites/apps.py
+++ sites/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class SitesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sites'
+    verbose_name = _('Sites')

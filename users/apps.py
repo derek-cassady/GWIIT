5fafc54edceeecbddaa27b38f00b13c87de99656
Derek Cassady <derek@cassadyweb.com>
2024-09-25 15:44:23 -0400

    Added 'verbose_name' and 'gettext_lazy' where appropriate in 'users/apps.py'

--- users/apps.py
+++ users/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    verbose_name = _('Users')
 
     # Import signals when the app is ready
     def ready(self):

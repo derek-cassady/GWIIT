52bb2c6ba5063f68075a408a509141e3dc4f0e48
derek-cassady <derek@cassadyweb.com>
2025-01-26 11:35:07 -0500

    added basic routing to "authorization/urls.py"

---
+++ authorization/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views  # Import your custom views module
+
+app_name = 'authorization'
+
+urlpatterns = [
+    # Route for managing roles
+    path('roles/', views.roles_view, name='roles'),
+    # Route for managing permissions
+    path('permissions/', views.permissions_view, name='permissions'),
+]

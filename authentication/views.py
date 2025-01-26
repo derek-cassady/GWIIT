4f2b80e0d642391e55d5dbcdec1783032bc2f39b
derek-cassady <derek@cassadyweb.com>
2025-01-26 18:50:07 -0500

    added rough template to "authentication/views.py and login.html" so I can try testing functionality in organizations app.

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Replace with the appropriate success URL
+            return redirect("some_success_url")
+        else:
+            return HttpResponse("Invalid login")
+    return render(request, "authentication/login.html")

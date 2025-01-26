a1cc458dcf7eae411582f98976ece81b5dd91d11
derek-cassady <derek@cassadyweb.com>
2025-01-26 09:28:27 -0500

    corrected "authentication/urls.py" template names from "users/" to "authentication/"

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ app_name = 'authentication'
 
 urlpatterns = [
     # Login route
-    path('login/', views.login_view(template_name='users/login.html'), name='login'),
+    path('login/', views.login_view(template_name='authentication/login.html'), name='login'),
     # Logout route
-    path('logout/', views.logout_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', views.logout_view(template_name='authentication/logout.html'), name='logout'),
 ]

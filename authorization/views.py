748bc897e2d0344d3b5c965d45db8327e6d340e7
derek-cassady <derek@cassadyweb.com>
2025-01-26 19:37:04 -0500

    added rough template to "authorization/views.py, urls.py, roles.html and permissions.html" so I can try testing functionality in organizations app.

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def roles_view(request):
+    # Add any logic needed for rendering the roles page.
+    return render(request, 'authorization/roles.html')
+
+def permissions_view(request):
+    # Add any logic needed for rendering the permissions page.
+    return render(request, 'authorization/permissions.html')

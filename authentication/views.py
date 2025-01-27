94e9325e4e970cbf9042ee458e9e7541fcfffad0
derek-cassady <derek@cassadyweb.com>
2025-01-26 19:10:16 -0500

    added rough template to "authentication/views.py and logout.html" so I can try testing functionality in organizations app.

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 
 def login_view(request):
@@ -14,3 +14,9 @@ def login_view(request):
         else:
             return HttpResponse("Invalid login")
     return render(request, "authentication/login.html")
+
+def logout_view(request):
+    # Logs out the current user
+    logout(request)
+    # Redirect to the login page after logout
+    return redirect('authentication:login')

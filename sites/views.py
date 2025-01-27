6e8b12a10b77e29b2c22d049be228c808a4a42bf
derek-cassady <derek@cassadyweb.com>
2025-01-26 19:48:56 -0500

    added rough template to "sites/views.py, urls.py, sites_details.html and sites_list.html" so I can try testing functionality in organizations app.

--- sites/views.py
+++ sites/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Site  # Make sure you have a Site model in your app
 
-# Create your views here.
+def site_list(request):
+    # Fetch all sites for listing
+    sites = Site.objects.all()
+
+    # Render the site list template
+    return render(request, 'sites/site_list.html', {
+        'sites': sites,
+    })
+
+def site_detail(request):
+    # Fetch all sites for listing
+    sites = Site.objects.all()
+
+    # Render the site detail template
+    return render(request, 'sites/site_detail.html', {
+        'sites': sites,
+    })

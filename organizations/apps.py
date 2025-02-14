def225ea5c3000fc5d7cd71adec2fd4a6204ace7
derek-cassady <derek@cassadyweb.com>
2025-02-13 21:16:43 -0500

    added debug print statements to organizations/apps.py

--- organizations/apps.py
+++ organizations/apps.py
@@ -5,3 +5,7 @@ class OrganizationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'organizations'
     verbose_name = _('Organizations')
+
+    def ready(self):
+        print("DEBUG: AppConfig ready() running for organizations app")
+        print("DEBUG: organizations app initialization complete.")

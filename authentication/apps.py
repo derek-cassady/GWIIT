ee212353564dd3242adb037cf0503f2c88737f5f
derek-cassady <derek@cassadyweb.com>
2025-02-13 21:15:36 -0500

    added debug print statements to authentication/apps.py

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+def ready(self):
+        print("DEBUG: AppConfig ready() running for authentication app")
+        print("DEBUG: authentication app initialization complete.")

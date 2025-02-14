83259834ed3d1341fea40ee8fd713d73d7aa0266
derek-cassady <derek@cassadyweb.com>
2025-02-13 21:16:03 -0500

    added debug print statements to authorization/apps.py

--- authorization/apps.py
+++ authorization/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthorizationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authorization'
+
+def ready(self):
+        print("DEBUG: AppConfig ready() running for authorization app")
+        print("DEBUG: authorization app initialization complete.")

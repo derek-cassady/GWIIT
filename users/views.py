de8022fc003f1a0973bed4f083650b142b4e7ebe
derek-cassady <derek@cassadyweb.com>
2025-02-16 02:33:40 -0500

    removed final references to DJango's auth system and now referencing the custom authorization logic in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,14 +1,15 @@
 print("DEBUG: Starting to load views for users app...")
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, UpdateView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .forms import CustomUserChangeForm
 from .models import User
+from users.authentication import CustomLoginRequiredMixin, custom_login_required
 
-@login_required
+# Handles user profile updates.
+@custom_login_required
 def user_profile(request):
+
     user = request.user
     if request.method == 'POST':
         form = CustomUserChangeForm(request.POST, instance=user)
@@ -19,8 +20,10 @@ def user_profile(request):
         form = CustomUserChangeForm(instance=user)
     return render(request, 'users/user_profile.html', {'form': form})
 
-@login_required
+# Displays user management dashboard.
+@custom_login_required
 def user_management(request):
+
     users = User.objects.all()
     return render(request, 'users/user_management.html', {'users': users})
 
